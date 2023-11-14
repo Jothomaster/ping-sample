5de40fdbbf50d3b39c372fb8f04944fbf9e01b3e
Franciszek Jurkowski <f.jurkowski1@gmail.com>
2023-11-14 12:08:02 +0100

    trying to initialize sidewalk

--- sample/src/main.c
+++ sample/src/main.c
@@ -4,6 +4,12 @@
 #include <zephyr/devicetree/gpio.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/device.h>
+#include <sid_api.h>
+#include <app_ble_config.h>
+#include <app_subGHz_config.h>
+#include <sidewalk_version.h>
+#include <sidewalk_callbacks.h>
+#include <pal_init.h>
 
 #define  BTN0_NODE DT_ALIAS(sw0)
 #define MSG_SIZE 256
@@ -81,8 +87,35 @@ static void button_pressed(const struct device *dev, struct gpio_callback *cb,
         k_work_submit_to_queue(&k_sys_work_q, &main_log_queue.work);
 }
 
+struct sid_event_callbacks event_callbacks;
+
+struct sid_config default_config = {
+        .link_mask = SID_LINK_TYPE_1,
+        .time_sync_periodicity_seconds = 7200,
+        .callbacks = &event_callbacks,
+};
+
+static struct sid_handle *handle;
+
 int main(void)
 {
+        PRINT_SIDEWALK_VERSION();
+        if (application_pal_init()) {
+		LOG_ERR("Failed to initialze PAL layer for sidewalk applicaiton.");
+		//application_state_error(&global_state_notifier, true);
+		return 0;
+	}
+        default_config.link_config = app_get_ble_config();
+        //default_config.sub_ghz_link_config = app_get_sub_ghz_config();
+        sid_error_t err;
+        if((err = sidewalk_callbacks_set(NULL, &event_callbacks)) != SID_ERROR_NONE){
+                LOG_ERR("SETTING CALLBACKS FAILED: %d", err);
+                return 0;
+        }
+        if((err = sid_init(&default_config, &handle)) != SID_ERROR_NONE){
+                LOG_ERR("INITIALIZATION FAILED: %d", err);
+                return 0;
+        }
         int ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
 	if (ret != 0) {
 		printk("Error %d: failed to configure %s pin %d\n",
